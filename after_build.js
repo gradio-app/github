18aa1bcc997878eb71cc93b620c41b8328fa3d7f
pngwn <hello@pngwn.io>
2023-07-18 19:06:42 +0100

    add action

--- after_build.js
+++ after_build.js
@@ -1,68 +1,56 @@
-import path from "path";
-
-// import { readdir } from 'fs'
-import { readdir, mkdir, readFile, copyFile } from "fs/promises";
-
+import { mkdir, readFile, copyFile } from "fs/promises";
 import { dirname, join } from "path";
 import { fileURLToPath } from "url";
+
 import recursive_read_dir from "recursive-readdir";
+
+import { getPackagesSync } from "@manypkg/get-packages";
 
 const __dirname = dirname(fileURLToPath(import.meta.url));
 
 const action_target = (pkg) => join(__dirname, "actions", pkg);
 const action_src = (pkg) => join(__dirname, "packages", pkg);
 
-async function copy_dist(pkg) {
-  let files;
-  let pkg_src = action_src(pkg);
-  let pkg_dest = action_target(pkg);
-  try {
-    files = await recursive_read_dir(join(pkg_src, "dist"), ["node_modules"]);
-  } catch (e) {
-    console.error(e);
-  }
+async function copy_dist(pkg_name, pkg_dir) {
+	let files;
+	let pkg_src = action_src(pkg_name);
+	let pkg_dest = action_target(pkg_name);
+	try {
+		files = await recursive_read_dir(join(pkg_dir, "dist"), ["node_modules"]);
+	} catch (e) {
+		console.error(e);
+	}
 
-  await mkdir(join(pkg_dest, "dist"), {
-    recursive: true,
-  });
-  const meta = await copyFile(
-    join(pkg_src, "action.yml"),
-    join(pkg_dest, "action.yml")
-  );
+	await mkdir(join(pkg_dest, "dist"), {
+		recursive: true,
+	});
 
-  await Promise.all(
-    files.map((f) => {
-      const dest_file = f.replace(pkg_src, pkg_dest);
-      return copyFile(f, dest_file);
-    })
-  );
+	await copyFile(join(pkg_src, "action.yml"), join(pkg_dest, "action.yml"));
 
-  console.log(`"${pkg}" action folder created."`);
-}
+	await Promise.all(
+		files.map((f) => {
+			const dest_file = f.replace(pkg_src, pkg_dest);
+			return copyFile(f, dest_file);
+		})
+	);
 
-function read_file(f) {
-  return new Promise((res) => {
-    readFile(f, { encoding: "utf-8" }).then((d) =>
-      res([f, JSON.parse(d).name])
-    );
-  });
+	console.log(`"${pkg_name}" action folder created."`);
 }
 
 async function handle_packages() {
-  const pkgs = await recursive_read_dir(join(__dirname, "packages"), [
-    "node_modules",
-    "dist",
-  ]);
+	const { packages: pkgs } = getPackagesSync(process.cwd());
 
-  const meta = (
-    await Promise.all(
-      pkgs.filter((f) => f.endsWith("package.json")).map(read_file)
-    )
-  )
-    .filter(([_, name]) => name.startsWith("@gradio-action/"))
-    .map(([f, pkg]) => [f, pkg.replace("@gradio-action/", "")]);
+	console.log(pkgs);
+	const action_packages = pkgs
+		.filter((p) => p.packageJson.name.startsWith("@gradio-action/"))
+		.map((p) => ({
+			name: p.packageJson.name.replace("@gradio-action/", ""),
+			path: p.dir,
+		}));
 
-  await Promise.all(meta.map((v) => copy_dist(v[1])));
+	await Promise.all(
+		action_packages.map(({ name, path }) => copy_dist(name, path))
+	);
 }
 
 handle_packages();
--- packages/deploy-space/index.ts
+++ packages/deploy-space/index.ts
@@ -15,165 +15,165 @@ import type { RepoId, Credentials } from "@huggingface/hub";
 import k from "kleur";
 
 async function run() {
-  const {
-    hf_token,
-    user_name,
-    space_name,
-    space_type,
-    is_artifact,
-    path,
-    comment,
-    gh_token,
-  } = handle_inputs();
+	const {
+		hf_token,
+		user_name,
+		space_name,
+		space_type,
+		is_artifact,
+		path,
+		comment,
+		gh_token,
+	} = handle_inputs();
 
-  const cwd = process.env.GITHUB_WORKSPACE as string;
-  let _path = join(cwd, path);
+	const cwd = process.env.GITHUB_WORKSPACE as string;
+	let _path = join(cwd, path);
 
-  if (is_artifact) {
-    const artifact_client = artifact.create();
-    await artifact_client.downloadArtifact(path, path);
-  }
+	if (is_artifact) {
+		const artifact_client = artifact.create();
+		await artifact_client.downloadArtifact(path, path);
+	}
 
-  const files: string[] = await recursive_read_dir(_path);
-  const file_data: Array<[string, Buffer]> = await Promise.all(
-    files.map(read_files(_path))
-  );
+	const files: string[] = await recursive_read_dir(_path);
+	const file_data: Array<[string, Buffer]> = await Promise.all(
+		files.map(read_files(_path))
+	);
 
-  const repo: RepoId = {
-    name: `${user_name}/${space_name}`,
-    type: "space",
-  };
+	const repo: RepoId = {
+		name: `${user_name}/${space_name}`,
+		type: "space",
+	};
 
-  const credentials: Credentials = {
-    accessToken: hf_token,
-  };
+	const credentials: Credentials = {
+		accessToken: hf_token,
+	};
 
-  const x = k.cyan(user_name);
-  const y = k.cyan(space_name);
-  const formatted_repo = x + k.dim("/") + y;
+	const x = k.cyan(user_name);
+	const y = k.cyan(space_name);
+	const formatted_repo = x + k.dim("/") + y;
 
-  try {
-    core.info(`Trying to create ${formatted_repo}.`);
-    await createRepo({ repo, credentials });
-  } catch (e: any) {
-    if (e.statusCode === 409) {
-      core.info(`${formatted_repo} already exists. Skipping.`);
-    } else {
-      core.setFailed(`Could not create ${formatted_repo}.\n${e.data.message}`);
-    }
-  }
+	try {
+		core.info(`Trying to create ${formatted_repo}.`);
+		await createRepo({ repo, credentials });
+	} catch (e: any) {
+		if (e.statusCode === 409) {
+			core.info(`${formatted_repo} already exists. Skipping.`);
+		} else {
+			core.setFailed(`Could not create ${formatted_repo}.\n${e.data.message}`);
+		}
+	}
 
-  const readme = make_readme({ title: space_name, sdk: space_type });
+	const readme = make_readme({ title: space_name, sdk: space_type });
 
-  file_data.push(["README.md", Buffer.from(readme)]);
+	file_data.push(["README.md", Buffer.from(readme)]);
 
-  core.info(
-    `Committing ${file_data.length} file${
-      file_data.length === 1 ? "" : "s"
-    } to ${formatted_repo}.`
-  );
+	core.info(
+		`Committing ${file_data.length} file${
+			file_data.length === 1 ? "" : "s"
+		} to ${formatted_repo}.`
+	);
 
-  try {
-    await commit({
-      repo,
-      credentials,
-      title: "Upload space from github action",
-      //@ts-ignore
-      operations: file_data.map(([filename, data]) => ({
-        operation: "addOrUpdate",
-        path: filename,
-        content: new Blob([data], {}),
-      })),
-    });
-  } catch (e: any) {
-    console.log(e);
-    core.setFailed(`Commit failed.\n${e.message}`);
-  }
+	try {
+		await commit({
+			repo,
+			credentials,
+			title: "Upload space from github action",
+			//@ts-ignore
+			operations: file_data.map(([filename, data]) => ({
+				operation: "addOrUpdate",
+				path: filename,
+				content: new Blob([data], {}),
+			})),
+		});
+	} catch (e: any) {
+		console.log(e);
+		core.setFailed(`Commit failed.\n${e.message}`);
+	}
 
-  try {
-    create_comment(gh_token, comment, repo.name);
-  } catch (e) {}
+	try {
+		create_comment(gh_token, comment, repo.name);
+	} catch (e) {}
 
-  core.info("Space successfully updated.");
+	core.info("Space successfully updated.");
 }
 
 function read_files(path: string) {
-  return function (file: string): Promise<[string, Buffer]> {
-    return new Promise((res, rej) => {
-      readFile(file).then((data) => res([file.replace(`${path}/`, ""), data]));
-    });
-  };
+	return function (file: string): Promise<[string, Buffer]> {
+		return new Promise((res, rej) => {
+			readFile(file).then((data) => res([file.replace(`${path}/`, ""), data]));
+		});
+	};
 }
 
 function handle_inputs() {
-  const _hf_token = core.getInput("hf_token", {
-    required: true,
-    trimWhitespace: true,
-  });
+	const _hf_token = core.getInput("hf_token", {
+		required: true,
+		trimWhitespace: true,
+	});
 
-  let hf_token: `hf_${string}`;
+	let hf_token: `hf_${string}`;
 
-  if (_hf_token.startsWith("hf_")) {
-    hf_token = _hf_token as `hf_${string}`;
-  } else {
-    core.setFailed("Not a valid Hugging face token. Must start with 'hf_'.");
-    throw new Error();
-  }
+	if (_hf_token.startsWith("hf_")) {
+		hf_token = _hf_token as `hf_${string}`;
+	} else {
+		core.setFailed("Not a valid Hugging face token. Must start with 'hf_'.");
+		throw new Error();
+	}
 
-  const user_name = core.getInput("user_name", {
-    required: true,
-    trimWhitespace: true,
-  });
-  const space_name = core.getInput("space_name", {
-    required: true,
-    trimWhitespace: true,
-  });
-  const space_type = core.getInput("space_type", { trimWhitespace: true });
-  const path = core.getInput("path", { required: true, trimWhitespace: true });
-  const is_artifact = core.getBooleanInput("is_artifact", {
-    trimWhitespace: true,
-  });
+	const user_name = core.getInput("user_name", {
+		required: true,
+		trimWhitespace: true,
+	});
+	const space_name = core.getInput("space_name", {
+		required: true,
+		trimWhitespace: true,
+	});
+	const space_type = core.getInput("space_type", { trimWhitespace: true });
+	const path = core.getInput("path", { required: true, trimWhitespace: true });
+	const is_artifact = core.getBooleanInput("is_artifact", {
+		trimWhitespace: true,
+	});
 
-  let _space_type;
-  if (!["static", "gradio"].includes(space_type)) {
-    core.setFailed(
-      `'${space_type}' is not a supported space type. Only 'gradio' and 'static' are supported.`
-    );
-    throw new Error();
-  } else {
-    _space_type = space_type as "gradio" | "static";
-  }
+	let _space_type;
+	if (!["static", "gradio"].includes(space_type)) {
+		core.setFailed(
+			`'${space_type}' is not a supported space type. Only 'gradio' and 'static' are supported.`
+		);
+		throw new Error();
+	} else {
+		_space_type = space_type as "gradio" | "static";
+	}
 
-  const comment = core.getBooleanInput("comment", { required: true });
+	const comment = core.getBooleanInput("comment", { required: true });
 
-  const gh_token = core.getInput("gh_token") || "";
+	const gh_token = core.getInput("gh_token") || "";
 
-  return {
-    hf_token,
-    user_name,
-    space_name,
-    space_type: _space_type,
-    is_artifact,
-    path,
-    gh_token,
-    comment,
-  };
+	return {
+		hf_token,
+		user_name,
+		space_name,
+		space_type: _space_type,
+		is_artifact,
+		path,
+		gh_token,
+		comment,
+	};
 }
 
 run();
 
 function make_readme({
-  title,
-  sdk,
-  version,
-  app_file = "app.py",
+	title,
+	sdk,
+	version,
+	app_file = "app.py",
 }: {
-  title: string;
-  sdk: "gradio" | "static";
-  version?: string;
-  app_file?: string;
+	title: string;
+	sdk: "gradio" | "static";
+	version?: string;
+	app_file?: string;
 }) {
-  let content = `title: ${title} 
+	let content = `title: ${title} 
 emoji: 💩
 colorFrom: indigo
 colorTo: indigo
@@ -181,14 +181,14 @@ sdk: ${sdk}
 pinned: false
 `;
 
-  if (sdk === "gradio") {
-    content += `app_file: ${app_file}`;
-    if (version) {
-      content += `sdk_version: ${version}`;
-    }
-  }
+	if (sdk === "gradio") {
+		content += `app_file: ${app_file}`;
+		if (version) {
+			content += `sdk_version: ${version}`;
+		}
+	}
 
-  return `---
+	return `---
 ${content}
 ---
 `;
@@ -196,52 +196,52 @@ ${content}
 import { GetResponseDataTypeFromEndpointMethod } from "@octokit/types";
 
 async function create_comment(
-  gh_token: string,
-  comment: boolean,
-  repo: string
+	gh_token: string,
+	comment: boolean,
+	repo: string
 ) {
-  if (!comment || (comment && !gh_token)) {
-    return;
-  }
+	if (!comment || (comment && !gh_token)) {
+		return;
+	}
 
-  const pr_number = github.context.payload.pull_request?.number;
-  if (!pr_number) return;
+	const pr_number = github.context.payload.pull_request?.number;
+	if (!pr_number) return;
 
-  const comment_tag_pattern = `<!-- gradio-app/deploy-space -->`;
-  const message = `Preview space has been deployed to https://huggingface.co/spaces/${repo}`;
-  const body = `${message}\n${comment_tag_pattern}`;
+	const comment_tag_pattern = `<!-- gradio-app/deploy-space -->`;
+	const message = `Preview space has been deployed to https://huggingface.co/spaces/${repo}`;
+	const body = `${message}\n${comment_tag_pattern}`;
 
-  const octokit = github.getOctokit(gh_token);
+	const octokit = github.getOctokit(gh_token);
 
-  type ListCommentsResponseDataType = GetResponseDataTypeFromEndpointMethod<
-    typeof octokit.rest.issues.listComments
-  >;
-  let _comment: ListCommentsResponseDataType[0] | undefined;
+	type ListCommentsResponseDataType = GetResponseDataTypeFromEndpointMethod<
+		typeof octokit.rest.issues.listComments
+	>;
+	let _comment: ListCommentsResponseDataType[0] | undefined;
 
-  for await (const { data: comments } of octokit.paginate.iterator(
-    octokit.rest.issues.listComments,
-    {
-      ...github.context.repo,
-      issue_number: pr_number,
-    }
-  )) {
-    _comment = comments.find((comment) =>
-      comment?.body?.includes(comment_tag_pattern)
-    );
-    if (comment) break;
-  }
+	for await (const { data: comments } of octokit.paginate.iterator(
+		octokit.rest.issues.listComments,
+		{
+			...github.context.repo,
+			issue_number: pr_number,
+		}
+	)) {
+		_comment = comments.find((comment) =>
+			comment?.body?.includes(comment_tag_pattern)
+		);
+		if (comment) break;
+	}
 
-  if (_comment) {
-    await octokit.rest.issues.updateComment({
-      ...github.context.repo,
-      comment_id: _comment.id,
-      body,
-    });
-  } else {
-    await octokit.rest.issues.createComment({
-      ...github.context.repo,
-      issue_number: pr_number,
-      body,
-    });
-  }
+	if (_comment) {
+		await octokit.rest.issues.updateComment({
+			...github.context.repo,
+			comment_id: _comment.id,
+			body,
+		});
+	} else {
+		await octokit.rest.issues.createComment({
+			...github.context.repo,
+			issue_number: pr_number,
+			body,
+		});
+	}
 }
