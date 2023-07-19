d7e15a18ab99132041096e6792518d98021918ce
pngwn <hello@pngwn.io>
2023-07-19 14:00:35 +0100

    update ci

--- packages/get-pr-branch/index.ts
+++ packages/get-pr-branch/index.ts
@@ -6,6 +6,31 @@ async function run() {
 	const pr = getInput("pr");
 	const { repo, owner } = context.repo;
 
+	const artifact = await octokit.rest.actions.listWorkflowRunArtifacts({
+		owner,
+		repo,
+		run_id: context.runId,
+	});
+
+	const pr_artifact = artifact.data.artifacts.find((a) => a.name === "pr");
+
+	if (!pr_artifact) {
+		setFailed("Could not find PR artifact.");
+		return;
+	}
+
+	const pr_number = (
+		await octokit.rest.actions.downloadArtifact({
+			owner: owner,
+			repo: repo,
+			artifact_id: pr_artifact.id,
+			archive_format: "zip",
+		})
+	).data;
+
+	console.log(pr_number);
+
+	throw new Error("test");
 	try {
 		const {
 			repository: {
