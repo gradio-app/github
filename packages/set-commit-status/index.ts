a22de0c5ebafba76d282c86389ed249894e43f2c
pngwn <hello@pngwn.io>
2024-08-03 21:15:05 +0100

    add set-commit-status action

---
+++ packages/set-commit-status/index.ts
@@ -0,0 +1,32 @@
+import { getInput } from "@actions/core";
+import { context, getOctokit } from "@actions/github";
+
+async function run() {
+	const token = getInput("token");
+	const sha = getInput("sha");
+	const name = getInput("name") || "Unknown Workflow";
+	const url = getInput("url");
+	const run_id = getInput("run_id");
+
+	const octokit = getOctokit(token);
+
+	const workflow_run = await octokit.rest.actions.getWorkflowRun({
+		owner: context.repo.owner,
+		repo: context.repo.repo,
+		run_id: parseInt(run_id),
+	});
+
+	const message = "Skipped — No changes detected";
+
+	octokit.rest.repos.createCommitStatus({
+		owner: context.repo.owner,
+		repo: context.repo.repo,
+		sha,
+		state: "success",
+		description: message,
+		context: name,
+		target_url: url || workflow_run.data.html_url,
+	});
+}
+
+run();
